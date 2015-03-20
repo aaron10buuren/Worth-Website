544de2374286a350e7533d6a6aa2b74886c8bc67
aaron10buuren <aaron.tenbuuren@gmail.com>
2015-03-19 21:27:03 -0400

    MASSIVE UPDATE
    added multiple salaries
    monospace price font
    new JS to update salary

--- timer.js
+++ timer.js
@@ -1,58 +1,17 @@
-(function($) {
-    $.fn.countTo = function(options) {
-        // merge the default plugin settings with the custom options
-        options = $.extend({}, $.fn.countTo.defaults, options || {});
+var interval = 10;
+var secondsInDay = (1000 / interval) * (60 * 60 * 24 * 365);
 
-        // how many times to update the value, and how much to increment the value on each update
-        var loops = Math.ceil(options.speed / options.refreshInterval),
-            increment = (options.to - options.from) / loops;
+$(document).ready(function() {
+  $('span.timer').each(function() {
+    var timer = $(this);
+    var salary = timer.data('salary');
+    var increase = salary / secondsInDay;
 
-        return $(this).each(function() {
-            var _this = this,
-                loopCount = 0,
-                value = options.from,
-                interval = setInterval(updateTimer, options.refreshInterval);
-
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-                $(_this).html(value.toFixed(options.decimals));
-
-                if (typeof(options.onUpdate) == 'function') {
-                    options.onUpdate.call(_this, value);
-                }
-
-                if (loopCount >= loops) {
-                    clearInterval(interval);
-                    value = options.to;
-
-                    if (typeof(options.onComplete) == 'function') {
-                        options.onComplete.call(_this, value);
-                    }
-                }
-            }
-        });
-    };
-
-    $.fn.countTo.defaults = {
-        from: 0,  // the number the element should start at
-        to: 100,  // the number the element should end at
-        speed: 100000,  // how long it should take to count between the target numbers
-        refreshInterval: 10000,  // how often the element should be updated
-        decimals: 5,  // the number of decimal places to show
-        onUpdate: null,  // callback method for every time the element is updated,
-        onComplete: null,  // callback method for when the element finishes updating
-    };
-})(jQuery);
-
-jQuery(function($) {
-        $('.timer').countTo({
-            from: 0,
-            to: 2500,
-            speed: 600000,
-            refreshInterval: .00158549,
-            onComplete: function(value) {
-                console.debug(this);
-            }
-        });
+    setInterval(function() {
+      var value = parseFloat(timer.text());
+      var newValue = (value + increase);
+      var formattedValue = newValue.toFixed(7).slice(0, 8);
+      timer.text(formattedValue);
+    }, interval);
     });
+});
